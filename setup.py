5fc5091529953d7da449bef2757ce483aa9cc464
Anurag Singh <anurag756singh@gmail.com>
2024-12-28 18:26:24 +0530

    This is my second commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->list[str]:
+    '''
+    this function return the list
+    '''
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+
+    
+
+setup(
+    name="ml-project",
+    version='0.0.1',
+    author='Anurag',
+    author_email='anurag756singh@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
